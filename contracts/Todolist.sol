6c9da9aa1fe62c1766536d5f7d6e0beaf0c2fad9
gmustafa <gmustafa.mansoori@gmail.com>
2021-01-25 10:13:33 +0530

    set up browser block chain , and meta mask

--- contracts/Todolist.sol
+++ contracts/Todolist.sol
@@ -2,4 +2,21 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract todoList{
     uint public taskCount = 0;
+
+    struct task{
+        uint id;
+        string content;
+        bool complete;
+    }
+    mapping(uint => task) public tasks;
+
+    constructor() public{
+      createTask("hi gulam");
+    }
+
+    function createTask(string memory _content)public{
+        taskCount ++;
+        tasks[taskCount] = task(taskCount,_content,false);
+
+    }
 }
